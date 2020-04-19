59a9abadb938333a63369cba0f688b915c728968
brijeshdw <41039977+brijeshdw@users.noreply.github.com>
2020-04-19 17:15:53 +0530

    Create cons.java

---
+++ cons.java
@@ -0,0 +1,11 @@
+package main;
+
+class Cons{  
+//creating a default constructor  
+Cons(){System.out.println("Constructor is created");}  
+//main method  
+public static void main(String args[]){  
+//calling a default constructor  
+Cons b=new Cons();  
+}  
+}
